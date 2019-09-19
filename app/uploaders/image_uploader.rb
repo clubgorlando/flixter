e15be118d110800c65ad690716a6a3f88e81a039
clubgorlando <eric.gorland@gmail.com>
2019-09-19 03:22:50 +0000

    changed resize_to_fill to resize_to_fit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fill: [800, 350]
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -24,6 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
+  process resize_to_fit: [800, 350]
   # process scale: [200, 300]
   #
   # def scale(width, height)
