619c56db4d061093b16c62971101abe67e2a3e26
clubgorlando <eric.gorland@gmail.com>
2019-09-29 00:32:29 +0000

    enforcing student enrollment in a course before viewing lesson files

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,17 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
 
   def show
   end
 
   private
 
-
+  def require_enrolled_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Not Enrolled in this course!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
